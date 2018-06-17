a21c216b0f3817d7a0e67def89c2e1040afa30a1
thehiddeneye <abhishek2github@gmail.com>
2018-06-17 23:34:06 +0530

    added a method for creation of the next block

--- bc1.py
+++ bc1.py
@@ -17,3 +17,11 @@ class Block:
     @staticmethod
     def create_genesis_block():
         return Block(0,date.datetime.now(),"genesis block","0")
+    
+    @staticmethod
+    def next_block(last_block):
+        this_index = last_block.index+1
+        this_timestamp = date.datetime.now()
+        this_data = "this is data" +str(this_index)
+        this_hash = last_block.hash
+        return Block(this_index,this_timestamp,this_data,this_hash)
