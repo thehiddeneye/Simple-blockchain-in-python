2589226c3b8bbdbfc2f9cda3e76fa2536f6f56b5
thehiddeneye <abhishek2github@gmail.com>
2018-06-17 23:30:12 +0530

    added sha256 encryption

--- bc1.py
+++ bc1.py
@@ -8,3 +8,8 @@ class Block:
         self.data = data
         self.previous_hash =previous_hash
         self.hash =self.hash_block()
+
+    def hash_block (self):
+        encrypt=hasher.sha256()
+        encrypt.update(str(self.index)+str(self.timestamp)+str(self.data)+str(self.previous_hash))
+        return encrypt.hexdigest()
